045338ab485d165d64f098da2820a77d00b64007
Anik200338 <sarkaranik200338@gmail.com>
2024-08-17 14:06:27 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://assignment-12-2d5b0.web.app',
+      'https://assignment-12-2d5b0.firebaseapp.com',
+    ],
+  })
+);
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scvnlgi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+    // Ensure the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('task is sitting');
 });
